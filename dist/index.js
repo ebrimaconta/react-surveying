67f2ff80833d147db96944061f1eb221f93e664d
Ebrima <ebrima@phenopolis.com>
2023-11-11 09:29:57 +0000

    fixed tsconfig issue

--- dist/index.js
+++ dist/index.js
@@ -32,7 +32,7 @@ function styleInject(css, ref) {
 var css_248z = ".poll{align-items:center;border-radius:5px;display:flex;flex-direction:column;font-family:inherit;padding:10px}.poll *{box-sizing:border-box}.poll .question{border-bottom:1px solid #6d4b943b;color:#3a3a3a;font-weight:400;margin:0;padding-bottom:3px;text-align:inherit}.poll .answers{align-items:center;display:flex;flex-direction:column;justify-content:center;list-style:none;padding:0}.poll .answers,.poll .answers li{margin:5px 0;width:100%}.poll .answers .option,.poll .answers .result{background-color:inherit;border:2px solid #000;border-radius:5px;cursor:pointer;font-family:inherit;font-size:100%;font-weight:700;line-height:1.15;margin:0;outline:0;padding:10px;transition:all .3s ease;width:100%}.poll .answers .option:active,.poll .answers .option:hover{background:#3030303b}.poll .answers .option.purple:active,.poll .answers .option.purple:hover{background:#6d4b943b}.poll .answers .option.red:active,.poll .answers .option.red:hover{background:#ff28283b}.poll .answers .option.blue:active,.poll .answers .option.blue:hover{background:#283eff3b}.poll .answers .option.white:active,.poll .answers .option.white:hover{background:#ffffff3b}.poll .answers .option.cyan:active,.poll .answers .option.cyan:hover{background:#00bcdd3b}.poll .answers .result{cursor:default;height:42px;padding:0;position:relative}.poll .answers .result .fill{align-items:center;border-radius:3px;display:flex;margin-top:10px;transition:all .5s ease;width:0}.poll .answers .result .labels{font-size:16px;margin:0 10px;position:absolute;top:10px}.poll .answers .result .answer{margin-left:15px;overflow:hidden;text-overflow:ellipsis;white-space:nowrap}.poll .answers .result .answer.vote:after{content:\" \\2713\"}.poll .votes{color:#3030305e;font-size:13px;margin:0;padding:0;text-align:inherit}";
 styleInject(css_248z);
 
-const themes = {
+var themes = {
     purple: ['#6D4B94', '#7C6497', '#6D4B943B'],
     red: ['#E23D3D', '#EF4545', '#FF28283B'],
     blue: ['#5674E0', '#5674E0', '#5674E03B'],
@@ -40,23 +40,24 @@ const themes = {
     white: ['#ffffff', '#ffffff', '#ffffff3B'],
     cyan: ['#00BCDD', '#00BCDD', '#00BCDD3B'],
 };
-const ReactSurvey = ({ question, answers, onVote, userEmail = '', listVoted = [], customStyles = {
-    questionSeparator: true,
-    questionSeparatorWidth: 'question',
-    questionBold: true,
-    questionColor: '#303030',
-    align: 'center',
-    theme: 'black',
-}, disable = false, }) => {
-    const [totalVotes, setTotalVotes] = React.useState(0);
-    const [voted, setVoted] = React.useState(false);
-    const calculatePercent = (votes, total) => {
+var ReactSurvey = function (_a) {
+    var question = _a.question, answers = _a.answers, onVote = _a.onVote, _b = _a.userEmail, userEmail = _b === void 0 ? '' : _b, _c = _a.listVoted, listVoted = _c === void 0 ? [] : _c, _d = _a.customStyles, customStyles = _d === void 0 ? {
+        questionSeparator: true,
+        questionSeparatorWidth: 'question',
+        questionBold: true,
+        questionColor: '#303030',
+        align: 'center',
+        theme: 'black',
+    } : _d, _e = _a.disable, disable = _e === void 0 ? false : _e;
+    var _f = React.useState(0), totalVotes = _f[0], setTotalVotes = _f[1];
+    var _g = React.useState(false), voted = _g[0], setVoted = _g[1];
+    var calculatePercent = function (votes, total) {
         if (votes === 0 && total === 0) {
             return '0%';
         }
-        return `${Math.round((votes / total) * 100)}%`;
+        return "".concat(Math.round((votes / total) * 100), "%");
     };
-    const alignPoll = (customAlign) => {
+    var alignPoll = function (customAlign) {
         if (customAlign === 'left') {
             return 'flex-start';
         }
@@ -67,25 +68,25 @@ const ReactSurvey = ({ question, answers, onVote, userEmail = '', listVoted = []
             return 'center';
         }
     };
-    const obtainColors = (customTheme) => {
-        const colors = themes[customTheme];
+    var obtainColors = function (customTheme) {
+        var colors = themes[customTheme];
         if (!colors) {
             return themes['black'];
         }
         return colors;
     };
-    const colors = obtainColors(customStyles.theme);
-    React.useEffect(() => {
-        setTotalVotes(answers.reduce((total, answer) => total + answer.votes, 0));
+    var colors = obtainColors(customStyles.theme);
+    React.useEffect(function () {
+        setTotalVotes(answers.reduce(function (total, answer) { return total + answer.votes; }, 0));
     }, []);
-    const vote = () => {
+    var vote = function () {
         if (!disable) {
             setTotalVotes(totalVotes + 1);
             setVoted(!voted);
         }
     };
-    const checkIfVoted = listVoted.includes(userEmail);
-    return (React.createElement("article", { className: `animate__animated animate__fadeIn animate__faster poll`, style: {
+    var checkIfVoted = listVoted.includes(userEmail);
+    return (React.createElement("article", { className: "animate__animated animate__fadeIn animate__faster poll", style: {
             textAlign: customStyles.align,
             alignItems: alignPoll(customStyles.align),
         } },
@@ -95,18 +96,18 @@ const ReactSurvey = ({ question, answers, onVote, userEmail = '', listVoted = []
                 fontWeight: customStyles.questionBold ? 'bold' : 'normal',
                 color: customStyles.questionColor,
             } }, question),
-        React.createElement("ul", { className: 'answers' }, answers.map((answer) => (React.createElement("li", { key: answer.option }, !checkIfVoted ? (React.createElement("button", { className: `animate__animated  animate__fadeIn animate__faster  option ${customStyles.theme}`, style: { color: colors[0], borderColor: colors[1] }, type: 'button', onClick: (e) => {
+        React.createElement("ul", { className: 'answers' }, answers.map(function (answer) { return (React.createElement("li", { key: answer.option }, !checkIfVoted ? (React.createElement("button", { className: "animate__animated  animate__fadeIn animate__faster  option ".concat(customStyles.theme), style: { color: colors[0], borderColor: colors[1] }, type: 'button', onClick: function (e) {
                 onVote(answer);
                 vote();
-            }, "aria-label": answer.option, disabled: disable }, answer.option)) : (React.createElement("div", { className: `animate__animated animate__fadeIn animate__faster result`, style: { color: colors[0], borderColor: colors[1], padding: '0px 15px' } },
+            }, "aria-label": answer.option, disabled: disable }, answer.option)) : (React.createElement("div", { className: "animate__animated animate__fadeIn animate__faster result", style: { color: colors[0], borderColor: colors[1], padding: '0px 15px' } },
             React.createElement("div", { className: 'fill', style: {
                     width: calculatePercent(answer.votes, totalVotes),
                     backgroundColor: colors[2],
                 } }),
             React.createElement("div", { className: 'label' },
                 React.createElement("span", { className: 'percent', style: { color: colors[0] } }, calculatePercent(answer.votes, totalVotes)),
-                React.createElement("span", { className: `answer ${voted ? 'vote' : ''}`, style: { color: colors[0] } }, answer.option)))))))),
-        React.createElement("p", { className: 'votes' }, `${totalVotes} vote${totalVotes !== 1 ? 's' : ''}`)));
+                React.createElement("span", { className: "answer ".concat(voted ? 'vote' : ''), style: { color: colors[0] } }, answer.option)))))); })),
+        React.createElement("p", { className: 'votes' }, "".concat(totalVotes, " vote").concat(totalVotes !== 1 ? 's' : ''))));
 };
 
 exports.ReactSurvey = ReactSurvey;
